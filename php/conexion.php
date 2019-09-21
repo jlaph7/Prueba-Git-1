87537989027ecdc0fcff18b9b3cf79576c5d1c75
Jlaph <jlaph.7@gmail.com>
2019-09-21 11:01:46 -0500

    Script bd, conexion y carpeta js

---
+++ php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$drive 	= "mysql";
+$server	= "localhost";
+$base 	= "bd_incidencia";
+$usuario 	= "root";
+$contrasena = "";
+$cadena		="$drive:host=$server;dbname=$base";
+
+try {
+    $cnx = new PDO($cadena,$usuario,$contrasena);
+} catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
