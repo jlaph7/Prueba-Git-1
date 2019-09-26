a56e739fe574cf199386021f52254a6c6d683068
51lv3r28 <jrp8900@gmail.com>
2019-09-26 14:00:57 -0500

    js/maps

--- js/maps.js
+++ js/maps.js
@@ -12,6 +12,39 @@ var map, infoWindow, marker;
 //     initMap();
 
 // }
+var controlUI;
+function CenterControl(controlDiv, map) {
+
+    // Set CSS for the control border.
+    controlUI = document.createElement('img');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click para detectar ubicación';
+    controlUI.src='images/maps/1x/personpin.png';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    // var controlText = document.createElement('div');
+    // controlText.style.color = 'rgb(25,25,25)';
+    // controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    // controlText.style.fontSize = '16px';
+    // controlText.style.lineHeight = '38px';
+    // controlText.style.paddingLeft = '5px';
+    // controlText.style.paddingRight = '5px';
+    // //controlText.innerHTML = 'Ubicación';
+    // controlUI.appendChild(controlText);
+
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function () {
+        Localizarme();
+    });
+
+}
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -19,7 +52,7 @@ function initMap() {
             lat: -12.046,
             lng: -77.042
         },
-        zoom: 8,
+        zoom: 12,
         disableDefaultUI: true,
         zoomControl: true,
         mapTypeControl: false,
@@ -28,10 +61,19 @@ function initMap() {
         rotateControl: false,
         fullscreenControl: false
     });
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+
+    centerControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+
+    //Localizarme();
 
 
+}
 
-    var ubitexto = 'Usted se encuentra aqui';
+function Localizarme() {
+    var ubitexto = '¿Se encuentra aqui?';
     infoWindow = new google.maps.InfoWindow({
         content: ubitexto
     });
@@ -48,7 +90,7 @@ function initMap() {
                 lng: longitud
 
             };
-            var imagen='images/maps/1x/personpin.png';
+            var imagen = 'images/maps/1x/personpin.png';
             marker = new google.maps.Marker({
                 position: pos,
                 map: map,
@@ -65,32 +107,30 @@ function initMap() {
 
             map.setCenter(pos);
 
-            document.getElementById("plat").innerHTML=latitud;
-            document.getElementById("plng").innerHTML=longitud;
+            document.getElementById("plat").innerHTML = latitud;
+            document.getElementById("plng").innerHTML = longitud;
 
 
         }, function () {
             //Localizacion no encontrada
             handleLocationError(true, infoWindow, map.getCenter());
-            map.addListener('click', function(e) {
+            map.addListener('click', function (e) {
                 placeMarkerAndPanTo(e.latLng, map);
-              });
+            });
 
             function placeMarkerAndPanTo(latLng, map) {
-              marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-              });
-              map.panTo(latLng);
+                marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map
+                });
+                map.panTo(latLng);
             }
         });
     } else {
         // Navegador no acepta gelocación
         handleLocationError(false, infoWindow, map.getCenter());
     }
-
 }
-
 
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
